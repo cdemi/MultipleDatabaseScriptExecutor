67a17c45613a09c8050e3447ba1e88bebe00dbc6
Christopher Demicoli <christopher.demicoli@outlook.com>
2014-12-09 19:25:16 +0100

    Coded Application

---
+++ App/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace App
+{
+    public class Config
+    {
+        public List<string> ConnectionStrings { get; set; }
+    }
+
+    internal class Program
+    {
+        private const string configFile = "config.json";
+
+        private static void Main()
+        {
+            if (File.Exists(configFile))
+            {
+                var config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(configFile));
+                string[] sqlFiles = Directory.GetFiles(".", "*.sql");
+
+                foreach (string sqlFile in sqlFiles)
+                {
+                    var sqlFileInfo = new FileInfo(sqlFile);
+                    string sql = File.ReadAllText(sqlFile);
+                    Console.WriteLine(sqlFileInfo.Name + ":");
+                    foreach (string connectionString in config.ConnectionStrings)
+                    {
+                        using (var sqlConnection = new SqlConnection(connectionString))
+                        {
+                            Console.Write(" - " + sqlConnection.Database);
+                            try
+                            {
+                                using (var sqlCommand = new SqlCommand(sql, sqlConnection))
+                                {
+                                    var stopwatch = new Stopwatch();
+                                    stopwatch.Start();
+                                    sqlConnection.Open();
+                                    sqlCommand.ExecuteNonQuery();
+                                    sqlConnection.Close();
+                                    stopwatch.Stop();
+                                    Console.WriteLine(": {0}", stopwatch.Elapsed);
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                string fileName = Path.GetFileNameWithoutExtension(sqlFileInfo.Name) + "_" +
+                                                  sqlConnection.Database + "_" + DateTime.Now.ToString("yyyyMMdd") +
+                                                  "_Error.txt";
+                                File.WriteAllText(fileName, ex.ToString());
+                                Console.WriteLine(": Error");
+                            }
+                        }
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Done. Press any key to exit.");
+            }
+            else
+            {
+                File.WriteAllText(configFile,
+                    JsonConvert.SerializeObject(new Config
+                    {
+                        ConnectionStrings = new List<string> {"ConnectionString1", "ConnectionString2"}
+                    }));
+                Console.WriteLine("You need to put in your connection strings in '{0}'", configFile);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# Introduction #
+This tool allows you to run SQL Queries across different databases. It may be useful for cases where you need to run some upgrade scripts.
+
+# Requirements #
+You need to have the following files in your root folder (relative to the executable)
+
+- `config.json`
+- At least one `*.sql` file
+
+## config.json ##
+The `config.json` file must be of the format:
+
+    {"ConnectionStrings":["ConnectionString1","ConnectionString2"]}
+
+## *.sql files ##
+You can have any amount of *.sql files in the root folder of your application. They will all be grabbed by the application and executed across all databases.
+
+# Usage #
+You can either download the solution and build it from source, or you can just download the binary file `MDSE.zip`.
+
+After you have downloaded (or compiled) these files, you can just execute `MDSE.exe`. The application will read `config.json` and any `*.sql` files located at the root of your application.
+
+# Errors #
+If the application encounters any errors, it will not stop execution. Rather it will skip the erroneous SQL file and move on to the next. An error file will be created at the root folder of the application with the name format `<SQLFileName>_<DatabaseName>_<Date>.txt`.
+
+# Updates #
+This by far can be improved a lot and is not a complete product in any way. If you want you can fork this or submit pull requests.
